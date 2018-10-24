8d52a8b540450d76cb58e4aa81571a2babab2db2
Aluno <aluno@Alunos-MacBook-Pro.local>
2018-10-23 20:47:50 -0400

    Comitando pjt triangulo

---
+++ triangulo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA;
+            int ladoB;
+            int ladoC;
+
+            string texto;
+            //comentando
+
+            Console.WriteLine("Digite um valor para o ladoA ");
+            ladoA = Int32.Parse(Console.ReadLine()) ;
+            texto = "ladoA: " + ladoA;
+
+            Console.WriteLine("Digite um valor para o ladoB ");
+            ladoB = Int32.Parse(Console.ReadLine());
+            texto += "\nladoB: " + ladoB ;
+
+            Console.WriteLine("Digite um valor para o ladoC ");
+            ladoC = Int32.Parse(Console.ReadLine());
+            texto += "\nladoC: " + ladoC;
+
+            Console.WriteLine(texto);
+
+        }
+    }
+}
